df43b020a6f1dc089227edc23434d1ba552c2b49
ddalla0425 <hiwgod92@gmail.com>
2025-07-12 23:26:16 +0900

    first commit: react setting

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          리엑트 프로젝트 준비
         </p>
         <a
           className="App-link"
